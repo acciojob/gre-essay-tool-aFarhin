4a19571b35c44174944a1030fccb156293d87c7d
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-31 16:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+function updateWordCount() {
+			let text = document.getElementById("evaluatedText").value.trim();
+			let wordCount = text.split(/\s+/).length;
+			document.getElementById("wordCount").innerHTML = wordCount;
+		}
