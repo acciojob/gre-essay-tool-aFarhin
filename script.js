40082952cb881e90863dfb639a90ad5c260660c1
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-31 17:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //your code here
-// function updateWordCount() {
-// 			let text = document.getElementById("evaluatedText").value.trim();
-// 			let wordCount = text.split(/\s+/).length;
-// 			document.getElementById("wordCount").innerHTML = wordCount;
-// 		}
+function updateWordCount() {
+			let text = document.getElementById("evaluatedText").value.trim();
+			let wordCount = text.split(/\s+/).length;
+			document.getElementById("wordCount").innerHTML = wordCount;
+		}
 
-function countWords() {
-  const text = document.getElementById("evaluatedText").value;
-  const wordCount = text.trim().split(/\s+/).length;
-  const displayCount = wordCount || 0; // if wordCount is falsy, display 0
-  document.getElementById("wordCount").innerHTML = displayCount;
-}
+// function countWords() {
+//   const text = document.getElementById("evaluatedText").value;
+//   const wordCount = text.trim().split(/\s+/).length;
+//   const displayCount = wordCount || 0; // if wordCount is falsy, display 0
+//   document.getElementById("wordCount").innerHTML = displayCount;
+// }
